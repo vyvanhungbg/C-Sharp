a644d38d0cb8dbde90537eb3edb20c4c1a77203d
Hung <vyvanhung2882001bg.com>
2022-03-31 07:47:35 +0700

    Bai5_PhieuBaiTap1_01/04/2022

---
+++ Base/VyVanHung_2019601093_proj51/PhieuBaiTap1_Bai5/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhieuBaiTap1_Bai5
+{
+    class Program
+    {
+        static List<ThiSinhA> list = new List<ThiSinhA>();
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+          
+            do
+            {
+
+                Console.WriteLine("\n1. Thêm sinh viên");
+                Console.WriteLine("2. Hiển thị danh sách các sinh viên");
+                Console.WriteLine("3. Hiển thị các sinh viên theo tổng điểm");
+                Console.WriteLine("4. Hiển thị các sinh viên theo địa chỉ");
+                Console.WriteLine("5. Tìm kiếm sinh viên theo SBD");
+                Console.WriteLine("6. Thoát.");
+               
+
+                Console.WriteLine("Nhập lựa chọn [1:6] :\n ");
+                int choose = int.Parse(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1: ThemSinhVien(); break;
+                    case 2: HienThi();break;
+                    case 3: HienThiTheoTongDiem();break;
+                    case 4: HienThiSinhVienTheoDiaChi();break;
+                    case 5: TimKiemTheoSoBaoDanh();break;
+                    case 6: Console.WriteLine("Đã thoát !"); return;
+                  
+                    default:
+                        Console.WriteLine("Nhập sai ! Nhập lại ");
+                        break;
+                }
+            } while (true);
+        }
+
+        private static void TimKiemTheoSoBaoDanh()
+        {
+            Console.WriteLine("Nhập số báo danh sinh viên cần tìm kiếm : ");
+            int soBaoDanh = int.Parse(Console.ReadLine());
+            foreach (ThiSinhA item in list)
+            {
+                if(item.SoBaoDanh == soBaoDanh)
+                {
+                    Console.WriteLine("Tìm thấy sinh viên với SBD "+soBaoDanh);
+                    ThiSinhA.Header();
+                    item.xuat();
+                    return;
+                }
+            }
+            Console.WriteLine("Không tìm thấy sinh viên nào có SBD "+soBaoDanh);
+        }
+
+        private static void HienThiSinhVienTheoDiaChi()
+        {
+            Console.WriteLine("Nhập địa chỉ :");
+            string diaChi = Console.ReadLine();
+            Console.WriteLine("Danh sách sinh viên có địa chỉ tại " + diaChi + " là :");
+            ThiSinhA.Header();
+            foreach (ThiSinhA item in list)
+            {
+                if (item.DiaChi.Equals(diaChi))
+                {
+                    item.xuat();
+                }
+            }
+        }
+
+        private static void HienThiTheoTongDiem()
+        {
+            Console.WriteLine("Nhập tổng điểm :");
+            double tongDiem = double.Parse(Console.ReadLine());
+            Console.WriteLine("Danh sách sinh viên có tổng điểm >= "+tongDiem+" là :");
+            ThiSinhA.Header();
+            foreach (ThiSinhA item in list)
+            {
+                if (item.TongDiem() >= tongDiem)
+                {
+                    item.xuat();
+                }
+            }
+        }
+
+        private static void HienThi()
+        {
+            Console.WriteLine("Danh sách sinh viên là : ");
+            ThiSinhA.Header();
+            foreach (ThiSinhA item in list)
+            {
+                item.xuat();
+            }
+        }
+
+        private static void ThemSinhVien()
+        {
+            ThiSinhA thiSinhA = new ThiSinhA();
+            Console.WriteLine("Nhập số báo danh : ");
+            thiSinhA.SoBaoDanh = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập họ tên : ");
+            thiSinhA.HoTen = Console.ReadLine();
+            Console.WriteLine("Nhập địa chỉ : ");
+            thiSinhA.DiaChi = Console.ReadLine();
+            Console.WriteLine("Nhập điểm toán : ");
+            thiSinhA.Toan = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập điểm lý : ");
+            thiSinhA.Ly = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập điểm hóa học : ");
+            thiSinhA.HoaHoc = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập điểm ưu tiên : ");
+            thiSinhA.DiemUuTien = Double.Parse(Console.ReadLine());
+            list.Add(thiSinhA);
+            Console.WriteLine("Thêm thành công !");
+        }
+    }
+}
---
+++ Base/VyVanHung_2019601093_proj51/PhieuBaiTap1_Bai5/ThiSinhA.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhieuBaiTap1_Bai5
+{
+    class ThiSinhA
+    {
+        private int soBaoDanh;
+        private string hoTen;
+        private string diaChi;
+        private double toan;
+        private double ly;
+        private double hoaHoc;
+        private double diemUuTien;
+       
+
+        public int SoBaoDanh { get => soBaoDanh; set => soBaoDanh = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public string DiaChi { get => diaChi; set => diaChi = value; }
+        public double Toan { get => toan; set => toan = value; }
+        public double Ly { get => ly; set => ly = value; }
+        public double HoaHoc { get => hoaHoc; set => hoaHoc = value; }
+        public double DiemUuTien { get => diemUuTien; set => diemUuTien = value; }
+       
+        public double TongDiem() { return toan + ly + hoaHoc + diemUuTien; }
+       
+
+        public ThiSinhA(int soBaoDanh, string hoTen, string diaChi, double toan, double ly, double hoaHoc, double diemUuTien)
+        {
+            SoBaoDanh = soBaoDanh;
+            HoTen = hoTen;
+            DiaChi = diaChi;
+            Toan = toan;
+            Ly = ly;
+            HoaHoc = hoaHoc;
+            DiemUuTien = diemUuTien;
+           
+        }
+
+        public ThiSinhA()
+        {
+        }
+
+        public static void Header()
+        {
+            string formatTable = String.Format("{0,-12} {1,-12} {2,-12} {3,-12:0.0} {4,-12:0.0}{5,-12:0.0} {6,-12:0.0} {7,-12:0.0}\n"
+                , "SoBaoDanh"," HoTen", "DiaChi", "Toan", "Ly", "HoaHoc", "DiemUuTien", "TongDiem");
+            Console.WriteLine(formatTable);
+        }
+        public void xuat()
+        {
+            string formatTable = String.Format("{0,-12} {1,-12} {2,-12} {3,-12:0.0} {4,-12:0.0}{5,-12:0.0} {6,-12:0.0} {7,-12:0.0}\n"
+                , SoBaoDanh,HoTen,DiaChi,Toan,Ly, HoaHoc,DiemUuTien,TongDiem());
+            Console.WriteLine(formatTable);
+        }
+    }
+}
